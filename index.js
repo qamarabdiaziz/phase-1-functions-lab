13823fc03f727c3acc24b35ef5567597a0ebda18
qamarabdiaziz <tushez98@gmail.com>
2023-03-20 18:51:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+
+function distanceFromInHqBlocks(someValue){
+    if (someValue<=42){
+        return 42-someValue
+
+    }
+    else {
+        return someValue-42
+    }
+
+
+
+}
+function distanceFromHqInFeet(someValue){
+    if (someValue>42){
+        return (someValue-42)*264
+    }
+    else {
+        return (42-someValue)*264
+    }
+}
+function distanceTravelledInFeet (start, end){
+   if(start>end){
+    return(start-end)*264
+   }
+   else {
+    return (end-start)*264
+   }
+}
+function calculatesFarePrice( start, end){
+    distanceTravelledInFeet(start, end)
+    if((end-start)*264<={
+        return: "This is one is on me"
+    })
+}
+
+else if (end-start) *264>400 (end -start)*264{
+    return(((end-start)*264)-400)*2
+
+}
+else if((end-start)*264>200) (end -start)*264{
+    return((end-start)*264>2500) {
+        return "cannot travel that far";
+
+    }
+    else {
+        return ""
+    }
+}
+
+
+
